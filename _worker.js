43b6c9cd6fc4090bb357dc83bcd46c60ea3677ba
he3699 <167159657+he3699@users.noreply.github.com>
2024-04-28 20:44:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -16,7 +16,7 @@ https://sub.xf.free.hr/auto
 `
 
 //请将机场订阅链接填入上方
-//let urls = [];// https://subs.zeabur.app/clash
+let urls = [];// https://subs.zeabur.app/clash
 //let urls = [];// https://subs.zeabur.app/clash , https://neko-warp.nloli.xyz/neko_warp.yaml
 let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
