9095866a9eb85aef5f181f421208e4d417bbbd5e
he3699 <167159657+he3699@users.noreply.github.com>
2024-04-28 12:03:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,18 +6,18 @@ let BotToken =''; //可以为空，或者@BotFather中输入/start，/newbot，�
 let ChatID =''; //可以为空，或者@userinfobot中获取，/start
 let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
 let FileName = 'CF-Workers-SUB';
-let SUBUpdateTime = 6; //自定义订阅更新时间，单位小时
+let SUBUpdateTime = 24; //自定义订阅更新时间，单位小时
 
 //节点链接 + 订阅链接
 let MainData = `
 vless://b7a392e2-4ef0-4496-90bc-1c37bb234904@cf.090227.xyz:443?encryption=none&security=tls&sni=edgetunnel-2z2.pages.dev&fp=random&type=ws&host=edgetunnel-2z2.pages.dev&path=%2F%3Fed%3D2048#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93t.me%2FCMLiussss%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
-//https://sub.xf.free.hr/auto
+https://sub.xf.free.hr/auto
 https://hy2sub.pages.dev
 `
 
 //请将机场订阅链接填入上方
-let urls = [];// https://subs.zeabur.app/clash , https://neko-warp.nloli.xyz/neko_warp.yaml
-
+let urls = [];// https://neko-warp.nloli.xyz/neko_warp.yaml
+//let urls = [];// https://subs.zeabur.app/clash , https://neko-warp.nloli.xyz/neko_warp.yaml
 let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 
